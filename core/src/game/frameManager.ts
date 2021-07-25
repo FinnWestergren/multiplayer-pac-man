4303e7cbe14c1552f1738070563f8483c9e2943d
FinnWestergren <fwesterg@skidmore.edu>
2021-07-25 10:01:43 -0400

    comments

--- core/src/game/frameManager.ts
+++ core/src/game/frameManager.ts
@@ -1,5 +1,12 @@
 import { IDEAL_FRAME_LENGTH } from ".";
 
+/*
+    Because javascript runs in an event loop and there is no garauntee on the length of a single frame,
+    I made this to track and "smoothen out" the user framerate experience. The "framerate" calculated here takes the average
+    length over the last 16 frames. This information is then used to effectively speed up or slow down the rate at which the actors
+    move. I believe this technique was used in DK 64, but I don't know what the name for this technique is. "Lag Compensation"?
+*/ 
+
 const trackerLength = 16;
 const divisor = 1 / trackerLength;
 
--- core/src/game/index.ts
+++ core/src/game/index.ts
@@ -2,12 +2,12 @@ import { ReduxStore } from "../types/redux";
 import { updateActors } from "./actorUpdater";
 import { getAverageFrameLength, updateFrameManager } from "./frameManager";
 
-export const IDEAL_FRAME_LENGTH = 1000 / 60; // 60 FPS
+export const IDEAL_FRAME_LENGTH = 1000 / 60; // 60 FPS is ideal. It will not always stay at 60, which is why we need to use the frame manager
 export const CELLS_PER_MILLISECOND = 0.005;
 
 export let store: ReduxStore
 
-export const runGame: (_store: ReduxStore, updateInterval: (handler: TimerHandler, timeout?: number) => number) => void = (_store, updateInterval) => {
+export const runGame = (_store: ReduxStore, updateInterval: (handler: TimerHandler, timeout?: number) => number) => {
     store = _store;
     updateInterval(update, IDEAL_FRAME_LENGTH);
 };
